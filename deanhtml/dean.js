a223ded42aa87f579c4f0348e89ffb9db2c49200
DnnDRL <lorenzoandrea164@gmail.com>
2025-04-14 21:50:13 +0800

    deleted:    BG.png deleted:    dean.js renamed:    dean.html -> deanhtml/dean.html new file:   deanhtml/dean.js renamed:    pic/dean.png -> deanhtml/dean.png new file:   deanhtml/deancert/cert1.png new file:   deanhtml/deancert/cert2.png new file:   deanhtml/deancert/cert3.png new file:   deanhtml/deancert/cert4.png renamed:    deanna.css -> deanhtml/deanna.css new file:   deanhtml/deanproject/UA Form.JPG new file:   deanhtml/deanproject/androidstudio.PNG new file:   deanhtml/deanproject/form.JPG new file:   deanhtml/deanproject/login.JPG new file:   deanhtml/deanproject/portfolio.PNG new file:   deanhtml/deanproject/resume.JPG renamed:    gab.html -> gabhtml/gab.html modified:   home.html renamed:    itiboy.html -> itiboyhtml/itiboy.html renamed:    martin.html -> martinhtml/martin.html new file:   pf/1.png new file:   pf/2.png new file:   pf/3.png new file:   pf/4.png new file:   pf/5.png new file:   pf/6.png renamed:    rannie.html -> ranniehtml/rannie.html renamed:    ziven.html -> zivenhtml/ziven.html

---
+++ deanhtml/dean.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  const menu = document.getElementById('menu-dropdown');
+  menu.classList.toggle('show');
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.menu-toggle')) {
+    const dropdown = document.getElementById('menu-dropdown');
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+};
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
